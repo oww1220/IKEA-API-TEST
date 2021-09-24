1b8f1b2e10b5bcbfed9ed33825d54958be00400c
lee dong girl <0193100266@naver.com>
2021-09-24 13:08:56 +0900

    [create] babel config

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,12 @@ const pxtorem = require('gulp-pxtorem');
 const cssnano = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
 
+/* babel */
+const concat = require('gulp-concat');
+const changed = require('gulp-changed');
+const babel = require('gulp-babel');
+const babel_polyfill_url = './node_modules/@babel/polyfill/browser.js';
+
 /*오류 처리*/
 const plumber = require('gulp-plumber');
 
@@ -28,6 +34,24 @@ const plumberOption = {
 
 const URL = 'posfeed';
 const TASK_BASE_URL = `./wwwroot/${URL}/assets`;
+
+
+gulp.task('babel', ()=> 
+    gulp
+    .src(['./wwwroot/lets_play/en/js/**/*.js'], {
+        sourcemaps: true
+    })
+    .pipe(
+        babel()
+    )
+    .pipe(concat('common_quizgame.js'))
+    .pipe(
+        gulp.dest('./wwwroot/lets_play/en/', {
+            sourcemaps: './maps'
+        })
+    )
+    .pipe(browserSync.reload({ stream: true }))
+);
 
 gulp.task('sass', ()=>
     gulp
@@ -71,11 +95,17 @@ gulp.task('watch', ()=> {
         `${TASK_BASE_URL}/scss/**/*.scss`,
         gulp.series('sass')
     );
+
+    // watch babel
+    gulp.watch(
+        `./wwwroot/lets_play/en/js/**/*.js`,
+        gulp.series('babel')
+    );
     
 });
 
 // task 묶어서 실행
 gulp.task(
     'default',
-    gulp.series('sass', 'watch')
+    gulp.series('sass','babel', 'watch')
 );
--- wwwroot/lets_play/en/js/client.js
+++ wwwroot/lets_play/en/js/client.js
@@ -1,12 +1,10 @@
-"use strict";
-
-$(function () {
+$(()=>{
   // 임시
   $(document).on({
-    click: function click() {
+    click : function(){
       $(this).closest('.intro').removeClass('active').siblings('.step1').addClass('active');
     }
-  }, '.intro .btnArea .startBtn');
+  },'.intro .btnArea .startBtn');
   /*
   // 정답 일 경우
   $(document).on({
@@ -14,21 +12,26 @@ $(function () {
       $(this).closest('.contList').addClass('active');
     }
   },'.step1 .chBtn'); */
+
   // 오답 일 경우
-
   $(document).on({
-    click: function click() {
+    click : function(){
       $(this).addClass('notAc');
-      setTimeout(function () {
+      setTimeout(function(){
         $('.step1').removeClass('active').siblings('.step2').addClass('active');
-      }, 1000);
+      },1000);
     }
-  }, '.step1 .chBtn'); // Replay 버튼 클릭 시
+  },'.step1 .chBtn');
 
+
+  // Replay 버튼 클릭 시
   $(document).on({
-    click: function click() {
+    click : function(){
       $(this).closest('.complete').removeClass('active').siblings('.intro').addClass('active');
     }
-  }, '.complete .btnArea .replayBtn');
+  },'.complete .btnArea .replayBtn');
+
 });
-//# sourceMappingURL=maps/common_quizgame.js.map
+
+
+
